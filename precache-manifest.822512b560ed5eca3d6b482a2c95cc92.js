670568f03ec3b78c9bc9243926db93cde5e9cb75
Parth Mantri <parth.mantri@quantiphi.com>
2020-08-14 17:37:01 +0530

    Updates

---
+++ precache-manifest.822512b560ed5eca3d6b482a2c95cc92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e087b3c8c5393d65b2d8952ac000bfe",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f6105b16adf36a6bd7ba",
+    "url": "/my-app/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "ca3c54d1390f1ed79d72",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f6105b16adf36a6bd7ba",
+    "url": "/my-app/static/js/2.d9da68e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.d9da68e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca3c54d1390f1ed79d72",
+    "url": "/my-app/static/js/main.49902c06.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
